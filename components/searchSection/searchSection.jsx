09f96bf5de8848cb6526b69e274def2f5aa33f9b
sadegh hosseini <thesadegh.h@gmail.com>
2022-01-15 23:46:25 +0330

    complete autoComplete

--- components/searchSection/searchSection.jsx
+++ components/searchSection/searchSection.jsx
@@ -1,17 +1,23 @@
-import { MicrophoneIcon, SearchIcon } from "@heroicons/react/outline";
+import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
 function SearchSection() {
 
-  const [inputVal, setInputVal] = useState(null);
+  const [inputVal, setInputVal] = useState({
+    constant: '',
+    dynamic: ''
+  });
   const [autoCompleteVals, setAutoCompleteVals] = useState();
   const [cursor, setCursor] = useState(0);
   const router = useRouter();
 
   const handleInput = async (e) => {
-    setInputVal(e.target.value);
+    setInputVal({
+      dynamic: e.target.value,
+      constant: e.target.value
+    });
     const data = await fetch(`https://www.google.com/complete/search?q=${e.target.value}&gl=us&hl=en&client=chrome`)
       .then(res => res.json())
       .catch(err => console.log(err));
@@ -20,20 +26,26 @@ function SearchSection() {
   };
 
   const searchAutocomplete = (val) => {
-    setInputVal(val);
+    setInputVal({
+      ...inputVal,
+      dynamic: val
+    });
     router.push(`/search?term=${val}&start=${0}`);
   };
 
 
   const search = (e) => {
-    if (inputVal && e.key == 'Enter') {
-      router.push(`/search?term=${inputVal}&start=${0}`);
+    if (inputVal.dynamic && e.key == 'Enter') {
+      router.push(`/search?term=${inputVal.dynamic}&start=${0}`);
     }
 
     if (e.key == 'ArrowDown') {
       setCursor(prev => (prev + 1));
       autoCompleteVals.map((item, i) => (
-        (i == cursor ? setInputVal(item) : '')
+        (i == cursor ? setInputVal({
+          ...inputVal,
+          dynamic: item
+        }) : '')
       ));
       if (autoCompleteVals.length == cursor) {
         setCursor(0);
@@ -43,7 +55,10 @@ function SearchSection() {
     if (e.key == 'ArrowUp') {
       setCursor(prev => (prev - 1));
       autoCompleteVals.map((item, i) => (
-        (i == cursor - 2 ? setInputVal(item) : '')
+        (i == cursor - 2 ? setInputVal({
+          ...inputVal,
+          dynamic: item
+        }) : '')
       ));
       if (cursor == 1) {
         setCursor(9);
@@ -52,8 +67,8 @@ function SearchSection() {
   };
 
   const searchWithClick = () => {
-    if (inputVal) {
-      router.push(`/search?term=${inputVal}&start=${0}`);
+    if (inputVal.dynamic) {
+      router.push(`/search?term=${inputVal.dynamic}&start=${0}`);
     }
   };
 
@@ -63,24 +78,30 @@ function SearchSection() {
       <Image src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
         width={300} height={100} objectFit="contain" />
       <div className="mx-auto w-full bg-white max-w-2xl relative">
-        <div className={`flex items-center ${inputVal ? 'rounded-t-3xl border-b-0' : 'rounded-full'} border focus-within:shadow-lg hover:shadow-lg mt-7 px-4`}>
+        <div className={`flex items-center ${inputVal.dynamic ? 'rounded-t-3xl border-b-0' : 'rounded-full'} border focus-within:shadow-lg hover:shadow-lg mt-7 px-4`}>
           <SearchIcon onClick={searchWithClick} className="h-5 text-gray-400 cursor-pointer" />
-          <input onChange={handleInput} onKeyDown={search} value={inputVal} className="outline-none flex-grow p-3 rounded-full w-full" type="text" />
+          <input onChange={handleInput} onKeyDown={search} value={inputVal.dynamic} className="outline-none flex-grow p-3 rounded-full w-full" type="text" />
+          {
+            inputVal.dynamic &&
+            <XIcon onClick={() => setInputVal({
+              constant: '',
+              dynamic: ''
+            })} className="h-7 text-gray-600 px-1 mr-2 border-gray-300 cursor-pointer border-r" />
+          }
           <MicrophoneIcon className="h-5 cursor-pointer" />
         </div>
-        <div className={`w-full rounded-b-3xl ${inputVal ? 'shadow-lg bg-white border border-t-0' : 'bg-none'} absolute rounded-b-3xl max-w-2xl `}>
+        <div className={`w-full rounded-b-3xl ${inputVal.dynamic ? 'shadow-lg bg-white border border-t-0' : 'bg-none'} absolute rounded-b-3xl max-w-2xl `}>
           {
-            inputVal &&
+            inputVal.dynamic &&
             <>
               <div className="border-t mx-5" />
               {autoCompleteVals?.map((item, i) => (
                 <div key={i} onClick={() => searchAutocomplete(item)} className={`${cursor - 1 == i ? 'bg-gray-200' : 'bg-white'} flex items-center px-4 py-2 hover:bg-gray-200 cursor-default`}>
                   <SearchIcon onClick={searchWithClick} className="h-5 py-[0.65px] pr-3 text-gray-400 cursor-pointer" />
-                  <div className="flex">
-                    <span> {item.substr(item.indexOf(inputVal), inputVal.length)} </span>
-                    <span className="font-bold"> {item.substr(item.indexOf(inputVal) + inputVal.length)} </span>
+                  <div className="flex whitespace-pre-wrap">
+                    <span>{item.substr(item.indexOf(inputVal.constant), inputVal.constant.length)}</span>
+                    <span className="font-bold">{item.substr(item.indexOf(inputVal.constant) + inputVal.constant.length)}</span>
                   </div>
-                  {/* {item} */}
                 </div>
               ))}
             </>
